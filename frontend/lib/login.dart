68d98929f90b4d621d6fe2f33661c17fc6e9c690
Aditya Ladwa <ladwa.aditya@gmail.com>
2021-02-11 22:46:43 +0530

    Layout for login page

--- frontend/lib/login.dart
+++ frontend/lib/login.dart
@@ -3,12 +3,28 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Login",
-      home: Scaffold(
-        body: Center(
-          child: Text("This is a text"),
-        ),
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextFormField(
+              cursorColor: Theme.of(context).cursorColor,
+              decoration: InputDecoration(
+                  labelText: "Email", border: OutlineInputBorder()),
+            ),
+          ),
+          Padding(
+            padding:EdgeInsets.all(20.0),
+            child: TextFormField(
+              cursorColor: Theme.of(context).cursorColor,
+              decoration: InputDecoration(
+                  labelText: "Password", border: OutlineInputBorder()),
+            ),
+          ),
+        ],
       ),
     );
   }

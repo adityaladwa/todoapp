17331edd4eaab946ef0d364ac905221e1b971085
Aditya Ladwa <ladwa.aditya@gmail.com>
2021-02-11 20:40:13 +0530

    Using material app widget for login

--- frontend/lib/login.dart
+++ frontend/lib/login.dart
@@ -3,9 +3,13 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Center(
-      child: Text("This is a text"),
+    return MaterialApp(
+      title: "Login",
+      home: Scaffold(
+        body: Center(
+          child: Text("This is a text"),
+        ),
+      ),
     );
   }
 }
